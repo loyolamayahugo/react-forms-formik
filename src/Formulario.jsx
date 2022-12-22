0a65ddc3eb0372c94e1d57d18a474d7e986286e8
loyolamayahugo <hugo.loyola@wizeline.com>
2022-12-22 10:48:59 -0600

    Other way to do the form

--- src/Formulario.jsx
+++ src/Formulario.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { useState } from 'react';
-import { Formik } from "formik";
+import { useState } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
 export const Formulario = () => {
-
-    const[formularioEnviado, cambiarFormularioEnviado] = useState( false );
+  const [formularioEnviado, cambiarFormularioEnviado] = useState(false);
 
   return (
     <div>
@@ -13,36 +12,47 @@ export const Formulario = () => {
           nombre: "",
           correo: "",
         }}
-
         validate={(valores) => {
-            let errores = {};
+          let errores = {};
 
-            /**Validacion nombre */
-            if( !valores.nombre ){
-                errores.nombre = 'Por favor ingresa un nombre'
-            }else if(!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(valores.nombre)){
-                errores.nombre = 'El nombre solo puede contener letras y espacios'
-            }
+          /**Validacion nombre */
+          if (!valores.nombre) {
+            errores.nombre = "Por favor ingresa un nombre";
+          } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(valores.nombre)) {
+            errores.nombre = "El nombre solo puede contener letras y espacios";
+          }
 
-            /**Validacion correo */
-            if( !valores.correo ){
-                errores.correo = 'Por favor ingresa un correo electronico'
-            }else if(!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(valores.correo)){
-                errores.correo = 'El correo solo puede contener letras, numeros, puntos, guiones y guion bajo'
-            }
+          /**Validacion correo */
+          if (!valores.correo) {
+            errores.correo = "Por favor ingresa un correo electronico";
+          } else if (
+            !/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(
+              valores.correo
+            )
+          ) {
+            errores.correo =
+              "El correo solo puede contener letras, numeros, puntos, guiones y guion bajo";
+          }
 
-            return errores;
+          return errores;
         }}
-
-        onSubmit={(valores, {resetForm}) => {
-            resetForm();
-            console.log(valores);  
-            console.log("Formulario enviado");
-            cambiarFormularioEnviado(true);
-            setTimeout(() => cambiarFormularioEnviado(false), 2000);
+        onSubmit={(valores, { resetForm }) => {
+          resetForm();
+          console.log(valores);
+          console.log("Formulario enviado");
+          cambiarFormularioEnviado(true);
+          setTimeout(() => cambiarFormularioEnviado(false), 2000);
         }}
       >
-        {({ values, errors, touched, handleSubmit, handleChange, handleBlur }) => (
+       {/**Esto es una manera de hacerlo, usando el evento handleSubmit*/}
+        {/* {({
+          values,
+          errors,
+          touched,
+          handleSubmit,
+          handleChange,
+          handleBlur,
+        }) => (
           <form className="formulario" onSubmit={handleSubmit}>
             <div>
               <label htmlFor="nombre">Nombre</label>
@@ -55,7 +65,9 @@ export const Formulario = () => {
                 onChange={handleChange}
                 onBlur={handleBlur}
               />
-              {touched.nombre && errors.nombre && <div className="error">{errors.nombre}</div>}
+              {touched.nombre && errors.nombre && (
+                <div className="error">{errors.nombre}</div>
+              )}
             </div>
             <div>
               <label htmlFor="correo">Correo</label>
@@ -68,11 +80,69 @@ export const Formulario = () => {
                 onChange={handleChange}
                 onBlur={handleBlur}
               />
-              {touched.correo && errors.correo && <div className="error">{errors.correo}</div>}
+              {touched.correo && errors.correo && (
+                <div className="error">{errors.correo}</div>
+              )}
             </div>
             <button type="submit">Enviar</button>
-            {formularioEnviado && <p className="exito">Formulario enviado con exito!</p>}
+            {formularioEnviado && (
+              <p className="exito">Formulario enviado con exito!</p>
+            )}
           </form>
+        )} */}
+        {({errors}) => (
+          <Form className="formulario">
+            <div>
+              <label htmlFor="nombre">Nombre</label>
+              <Field
+                type="text"
+                id="nombre"
+                name="nombre"
+                placeholder="john Doe"
+              />
+              <ErrorMessage name="nombre" component={() => (
+                <div className="error">{errors.nombre}</div>
+              )}/>
+            </div>
+            <div>
+              <label htmlFor="correo">Correo</label>
+              <Field
+                type="text"
+                id="correo"
+                name="correo"
+                placeholder="john@gmail.com"
+              />
+              <ErrorMessage name="correo" component={() => (
+                <div className="error">{errors.correo}</div>
+              )}/>
+            </div>
+
+            <div>
+                <Field name='pais' as="select">
+                    <option value='mexico'>Mexico</option>
+                    <option value='africa'>Africa</option>
+                    <option value='marruecos'>Marruecos</option>
+                </Field>
+            </div>
+
+            <div>
+                <label>
+                    <Field type='radio' name='sexo' value='hombre'/> Hombre
+                </label>
+                <label>
+                    <Field type='radio' name='sexo' value='hombre'/> Mujer
+                </label>
+            </div>
+
+            <div>
+                <Field name='mensaje' as='textarea' placeholder='Mensaje'/>
+            </div>
+
+            <button type="submit">Enviar</button>
+            {formularioEnviado && (
+              <p className="exito">Formulario enviado con exito!</p>
+            )}
+          </Form>
         )}
       </Formik>
     </div>
